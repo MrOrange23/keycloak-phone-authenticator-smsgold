59f0c0e395adae20701ec135477b5e9d537b9530
Timur Zarechinskiy <timur.zarechinskiy@aciworldwide.com>
2021-10-18 12:56:11 +0300

    initial commit

---
+++ src/main/java/org/onisfera/smsgold/SmsGoldServiceProviderFactory.java
@@ -0,0 +1,36 @@
+package org.onisfera.smsgold;
+
+import com.hfx.keycloak.spi.SmsService;
+import com.hfx.keycloak.spi.SmsServiceProviderFactory;
+import org.keycloak.Config.Scope;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class SmsGoldServiceProviderFactory implements SmsServiceProviderFactory {
+
+  @Override
+  public SmsService create(KeycloakSession keycloakSession) {
+    return new SmsGoldService(keycloakSession);
+  }
+
+  @Override
+  public void init(Scope scope) {
+
+  }
+
+  @Override
+  public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+  @Override
+  public String getId() {
+    return "StubbedSmsServiceProviderFactory";
+  }
+
+}
